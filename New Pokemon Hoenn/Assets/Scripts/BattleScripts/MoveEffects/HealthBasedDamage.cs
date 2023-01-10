f48ace9af68342683caf2be295eb70b13a156218
CDrury0 <cdrury922@gmail.com>
2023-01-10 03:12:03 -0500

    Continue populating move data
    - Also added damage formula for Water Spout / Eruption and extended that to Self-Destruct and Explosion (no more getting insta-killed by wayward voltorb)

--- New Pokemon Hoenn/Assets/Scripts/BattleScripts/MoveEffects/HealthBasedDamage.cs
+++ New Pokemon Hoenn/Assets/Scripts/BattleScripts/MoveEffects/HealthBasedDamage.cs
@@ -18,10 +18,16 @@ public class PiecewiseDamageData
 }
 public class HealthBasedDamage : NormalDamage
 {
+    public bool waterSpoutTypeDamage;
     public PiecewiseDamageData piecewiseDamage;
     public override IEnumerator DoEffect(BattleTarget user, BattleTarget target, MoveData moveData)
     {
-        int power = piecewiseDamage.GetPower((float)user.pokemon.CurrentHealth / (float)user.pokemon.stats[0]);
+        int power = waterSpoutTypeDamage ? WaterSpoutFormula(user, moveData.displayPower) : piecewiseDamage.GetPower((float)user.pokemon.CurrentHealth / (float)user.pokemon.stats[0]);
         yield return StartCoroutine(NormalDamageMethod(user, target, moveData, power));
+    }
+
+    private int WaterSpoutFormula(BattleTarget user, int displayPower){
+        int power = (int)((float)(displayPower) * (float)(user.pokemon.CurrentHealth) / (float)(user.pokemon.stats[0]));
+        return power > 1 ? power : 1;
     }
 }
