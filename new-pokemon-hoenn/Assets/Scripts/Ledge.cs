7f542dd96603a5aabd10fecb8c7140e52ee0b32f
Christian <cdrury922@gmail.com>
2025-04-13 18:07:24 -0400

    Made a simple ledge object creation pattern, and added a few to a route

---
+++ new-pokemon-hoenn/Assets/Scripts/Ledge.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    [SerializeField] private Vector3 _hopDirection;
+    public Vector3 HopDirection {get;}
+}
