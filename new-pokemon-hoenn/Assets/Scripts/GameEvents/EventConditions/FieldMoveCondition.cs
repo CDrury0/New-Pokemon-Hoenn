3eb5d158be4bc911b6427fa61388f86d1111bfe8
Christian <cdrury922@gmail.com>
2025-05-25 15:46:21 -0400

    Created working prototype cuttable tree

--- new-pokemon-hoenn/Assets/Scripts/GameEvents/EventConditions/FieldMoveCondition.cs
+++ new-pokemon-hoenn/Assets/Scripts/GameEvents/EventConditions/FieldMoveCondition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Event/FieldMoveCondition")]
 public class FieldMoveCondition : EventCondition
 {
     [SerializeField] private GameObject requiredMove;
--- new-pokemon-hoenn/Assets/Scripts/MiscDataModels/GymBadge.cs
+++ new-pokemon-hoenn/Assets/Scripts/MiscDataModels/GymBadge.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "GymBadge")]
 public class GymBadge : ScriptableObject
 {
     [SerializeField] private string _badgeName;
